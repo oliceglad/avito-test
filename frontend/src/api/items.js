3136b13eecad79d232a55605af7fda49761add46
oliceglad <vycheslavofficial@gmail.com>
2025-02-14 20:05:11 +0400

    changed api URL

--- frontend/src/api/items.js
+++ frontend/src/api/items.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const itemsApi = createApi({
   reducerPath: "itemsApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3000/api/items",
+    baseUrl: "http://localhost:3000/items",
   }),
   endpoints: (builder) => ({
     getAllItems: builder.query({
