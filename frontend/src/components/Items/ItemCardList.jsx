a0ea04ea8011ff0bc7297e763dd7852134132860
oliceglad <vycheslavofficial@gmail.com>
2025-02-14 20:20:46 +0400

    added loader

--- frontend/src/components/Items/ItemCardList.jsx
+++ frontend/src/components/Items/ItemCardList.jsx
@@ -1,9 +1,12 @@
 import s from "./Items.module.scss";
+import Skeleton from "@mui/material/Skeleton";
+import Stack from "@mui/material/Stack";
 import { ItemCard } from "./ItemCard";
+import { Loader } from "../UI/loader";
 
 export const ItemCardList = ({ items, filteredItems, isLoading, isError }) => {
   return (
-    <ul class>
+    <ul>
       {items ? (
         filteredItems.length > 0 ? (
           filteredItems.map((elem, key) => <ItemCard key={key} elem={elem} />)
@@ -11,7 +14,7 @@ export const ItemCardList = ({ items, filteredItems, isLoading, isError }) => {
           <div>Ничего не найдено</div>
         )
       ) : isLoading ? (
-        <div>Загрузка</div>
+        <Loader width="70px" height="70px" />
       ) : (
         isError ?? <div>Ошибка</div>
       )}
---
+++ frontend/src/components/UI/loader.jsx
@@ -0,0 +1,65 @@
+export const Loader = ({ width = "50px", height = "50px" }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 200 200"
+      width={width}
+      height={height}
+    >
+      <circle
+        fill="rgb(42, 42, 42)"
+        stroke="rgb(42, 42, 42)"
+        strokeWidth="15"
+        r="15"
+        cx="40"
+        cy="65"
+      >
+        <animate
+          attributeName="cy"
+          calcMode="spline"
+          dur="2"
+          values="65;135;65;"
+          keySplines=".5 0 .5 1;.5 0 .5 1"
+          repeatCount="indefinite"
+          begin="-.4"
+        ></animate>
+      </circle>
+      <circle
+        fill="rgb(42, 42, 42)"
+        stroke="rgb(42, 42, 42)"
+        strokeWidth="15"
+        r="15"
+        cx="100"
+        cy="65"
+      >
+        <animate
+          attributeName="cy"
+          calcMode="spline"
+          dur="2"
+          values="65;135;65;"
+          keySplines=".5 0 .5 1;.5 0 .5 1"
+          repeatCount="indefinite"
+          begin="-.2"
+        ></animate>
+      </circle>
+      <circle
+        fill="rgb(42, 42, 42)"
+        stroke="rgb(42, 42, 42)"
+        strokeWidth="15"
+        r="15"
+        cx="160"
+        cy="65"
+      >
+        <animate
+          attributeName="cy"
+          calcMode="spline"
+          dur="2"
+          values="65;135;65;"
+          keySplines=".5 0 .5 1;.5 0 .5 1"
+          repeatCount="indefinite"
+          begin="0"
+        ></animate>
+      </circle>
+    </svg>
+  );
+};
