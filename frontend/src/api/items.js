4b1e6e31b061db2c9fb9637f3d622457f5d70fbc
oliceglad <vycheslavofficial@gmail.com>
2025-02-14 19:39:48 +0400

    added basic api file with GET request

---
+++ frontend/src/api/items.js
@@ -0,0 +1,15 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const itemsApi = createApi({
+  reducerPath: "itemsApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3000/api/items",
+  }),
+  endpoints: (builder) => ({
+    getAllItems: builder.query({
+      query: () => ``,
+    }),
+  }),
+});
+
+export const { useGetAllItemsQuery } = itemsApi;
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
+import { itemsApi } from "../api/items";
 
 export const store = configureStore({
   reducer: {
+    [itemsApi.reducerPath]: itemsApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware(),
+    getDefaultMiddleware().concat(itemsApi.middleware),
 });
 
 setupListeners(store.dispatch);
