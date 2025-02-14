d0c7c28268b40281f7a6dab2dcd287c7627e5990
oliceglad <vycheslavofficial@gmail.com>
2025-02-14 18:51:54 +0400

    changed import mistake in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import "./index.scss";
 import App from "./App.jsx";
 
 import { Provider } from "react-redux";
-import { store } from "./redux/index.js";
+import { store } from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
