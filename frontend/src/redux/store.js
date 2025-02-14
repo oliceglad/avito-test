9a171dd51392ad7b66a07445f94a619c41b31aab
oliceglad <vycheslavofficial@gmail.com>
2025-02-14 18:44:14 +0400

    added basic redux to project

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+
+export const store = configureStore({
+  reducer: {
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware(),
+});
+
+setupListeners(store.dispatch);
