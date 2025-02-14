673985ab8b20a429ebbcfadad83281d8500b6e00
oliceglad <vycheslavofficial@gmail.com>
2025-02-14 22:07:44 +0400

    refactor ItemCardList

--- frontend/src/components/Items/ItemCardList.jsx
+++ frontend/src/components/Items/ItemCardList.jsx
@@ -2,20 +2,16 @@ import s from "./Items.module.scss";
 import { ItemCard } from "./ItemCard";
 import { Loader } from "../UI/loader";
 
-export const ItemCardList = ({ items, filteredItems, isLoading, isError }) => {
+export const ItemCardList = ({ items, isLoading, isError }) => {
+  if (isLoading) return <div className={s.ItemCardContainer}><Loader width="70px" height="70px" /></div>;
+  if (isError) return <div>Ошибка загрузки</div>;
+  if (!items || items.length === 0) return <div>Ничего не найдено</div>;
+
   return (
-    <ul className={s.ItemCardList}>
-      {items ? (
-        filteredItems.length > 0 ? (
-          filteredItems.map((elem, key) => <ItemCard key={key} elem={elem} />)
-        ) : (
-          <div>Ничего не найдено</div>
-        )
-      ) : isLoading ? (
-        <Loader width="70px" height="70px" />
-      ) : (
-        isError ?? <div>Ошибка</div>
-      )}
+    <ul className={s.ItemCardContainer}>
+      {items.map((elem) => (
+        <ItemCard key={elem.id} elem={elem} />
+      ))}
     </ul>
   );
 };
